aa01e84e52f254e509e5706e16078dd92e617fc0
r3czus <r3czusxd@gmail.com>
2023-09-05 09:04:33 +0200

    rozwiazanie problemu z testami
    .

--- ContainsDuplicate/ContainsDuplicate/Program.cs
+++ ContainsDuplicate/ContainsDuplicate/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ContainsDuplicate
 {
@@ -10,7 +12,30 @@ namespace ContainsDuplicate
     {
         static void Main(string[] args)
         {
-
+           // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
         }
+
+
     }
+    public class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+
+            Array.Sort(nums);
+            int n = nums.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (nums[i] == nums[i + 1])
+                    return true;
+            }
+            return false;
+        }
+
+
+
+
+
+    }
+
 }
