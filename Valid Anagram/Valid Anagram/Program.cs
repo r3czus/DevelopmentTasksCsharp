638c6c32e787d5ea3090d951fb09789dead41ca2
r3czus <r3czusxd@gmail.com>
2023-09-05 09:19:39 +0200

    dodanie rozwiązania ValidAnagram+testy
    .

--- Valid Anagram/Valid Anagram/Program.cs
+++ Valid Anagram/Valid Anagram/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,43 @@ namespace Valid_Anagram
     {
         static void Main(string[] args)
         {
+            //Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+            //An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
         }
+    }
+    public class Solution
+    {
+        public bool IsAnagram(string s, string t)
+        {
+
+            ArrayList sList = new ArrayList();
+            ArrayList tList = new ArrayList();
+            for (int i = 0; i < s.Length; i++)
+            {
+                sList.Add(s[i]);
+            }
+            for (int i = 0; i < t.Length; i++)
+            {
+                tList.Add(t[i]);
+            }
+            sList.Sort();
+            tList.Sort();
+
+            for (int i = 0; i < sList.Count; i++)
+            {
+                if (sList[i].Equals(tList[i]) && sList.Count == tList.Count)
+                {
+                    Console.WriteLine(sList[i] + "/" + tList[i]);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+
+
+        }
+
     }
 }
---
+++ Valid Anagram/ValidAnagramTest/ValidAnagramTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Valid_Anagram;
+
+namespace ValidAnagramTest
+{
+    [TestClass]
+    public class ValidAnagramTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution solution = new Solution();
+            string s = "anagram", t = "nagaram";
+
+            Assert.IsTrue(solution.IsAnagram(s, t));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution solution = new Solution();
+            string s = "rat", t = "cat";
+
+            Assert.IsFalse(solution.IsAnagram(s, t));
+        }
+    }
+}
